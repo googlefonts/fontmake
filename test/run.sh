80dac3e18dbc519b347029ddb4a9f4b5e8059690
James Godfrey-Kittle <jamesgk@google.com>
2016-06-20 16:36:12 -0700

    Add fontdiff step to integration test
    This adds some simple rendering tests to the integration test. Ideally
    we could add some more complex text to render (for example UDHR text),
    but there's actually some kerning differences between the golden data
    and fontmake output so that will have to wait (or, we could just add
    test cases we know will fail and then try to fix them very soon).

--- test/run.sh
+++ test/run.sh
@@ -5,3 +5,15 @@ fontmake -g 'NotoSansDevanagari/NotoSansDevanagari.glyphs'\
 for script in 'Ethiopic' 'Hebrew'; do
     fontmake -i -g "NotoSans${script}-MM.glyphs"
 done
+
+for script in 'Ethiopic' 'Hebrew'; do
+    family="NotoSans${script}"
+    ./fontdiff-linux --before "expected/${family}-Regular.otf"\
+        --after "instance_otf/${family}-Regular.otf"\
+        --specimen "${family}.html"\
+        --out "${family}.pdf"
+    if [[ $? = 1 ]]; then
+        echo "differences found in ${family} output"
+        exit 1
+    fi
+done
