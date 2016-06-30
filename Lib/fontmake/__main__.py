f00d675434b5e80ed7549f2e938dc62df38e6446
James Godfrey-Kittle <jamesgk@google.com>
2016-06-30 11:51:44 -0700

    Add a "family-name" parameter
    To support similar changes to glyphsLib:
    https://github.com/googlei18n/glyphsLib/pull/52

--- Lib/fontmake/__main__.py
+++ Lib/fontmake/__main__.py
@@ -27,6 +27,8 @@ def main():
     parser.add_argument('-i', '--interpolate', action='store_true',
                         help='interpolate masters (Glyphs source only)')
     parser.add_argument('--mti-source')
+    parser.add_argument('--family-name', help='Family name to use for masters,'
+                        'and to filter output instances by')
     parser.add_argument('--use-afdko', action='store_true')
     parser.add_argument('--keep-overlaps', dest="remove_overlaps",
                         action='store_false')
