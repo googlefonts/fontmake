a608d31e2b1550f27148f4c909145a0190a43072
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-12-20 17:54:18 +0000

    setup.py: add install_requires & PyPI metadata, read long_description from README.rst
    bump version to 1.1.0.dev0; will drop the pre-release suffix once we push v1.1.0 tag

--- setup.py
+++ setup.py
@@ -13,17 +13,50 @@
 # limitations under the License.
 
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from io import open
 
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='fontmake',
-    version='1.0',
-    packages=['fontmake'],
+    name="fontmake",
+    version="1.1.0.dev0",
+    description=("Compile fonts from sources (UFO, Glyphs) to binary "
+                 "(OpenType, TrueType)."),
+    long_description=long_description,
+    url="https://github.com/googlei18n/fontmake",
+    license="Apache Software License 2.0",
+    packages=find_packages("Lib"),
     package_dir={'': 'Lib'},
     entry_points={
         'console_scripts': [
             'fontmake = fontmake.__main__:main'
         ]
-    }
+    },
+    install_requires=[
+        "fonttools>=3.1.2",
+        "cu2qu>=1.1",
+        "glyphsLib>=1.1",
+        "ufo2ft>=0.2",
+        "MutatorMath>=2.0",
+        "defcon>=0.2",
+        "booleanOperations>=0.6",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+        "Topic :: Text Processing :: Fonts",
+    ],
 )
