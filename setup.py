206f8cdc079736ea865d6abe2ea719a6138e76e9
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-07 19:05:39 +0200

    Update ufo2ft from 0.4.1 to 0.4.2
    This will set IgnoreMarks by default in kernFeatureWriter
    (should fix https://github.com/googlei18n/fontmake/issues/273)

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "fonttools>=3.9.1",
         "cu2qu>=1.1.1",
         "glyphsLib>=1.6.0",
-        "ufo2ft>=0.4.1",
+        "ufo2ft>=0.4.2",
         "MutatorMath>=2.0.3",
         "defcon>=0.3.1",
         "booleanOperations>=0.7.0",
