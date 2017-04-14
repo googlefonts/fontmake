a800ab0358d342c916f46545e74aab65e4512d2b
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-14 22:26:56 +0100

    [setup.py] add 'bump_version' and 'release' commands
    same as https://github.com/googlei18n/ufo2ft/pull/128
    and https://github.com/googlei18n/glyphsLib/commit/6e50a1994d23f0c64487b584ec6dc6b6a000ab18

--- setup.py
+++ setup.py
@@ -12,10 +12,143 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-from setuptools import setup, find_packages
+import sys
+from setuptools import setup, find_packages, Command
+from distutils import log
 from io import open
 
+
+class bump_version(Command):
+
+    description = "increment the package version and commit the changes"
+
+    user_options = [
+        ("major", None, "bump the first digit, for incompatible API changes"),
+        ("minor", None, "bump the second digit, for new backward-compatible features"),
+        ("patch", None, "bump the third digit, for bug fixes (default)"),
+    ]
+
+    def initialize_options(self):
+        self.minor = False
+        self.major = False
+        self.patch = False
+
+    def finalize_options(self):
+        part = None
+        for attr in ("major", "minor", "patch"):
+            if getattr(self, attr, False):
+                if part is None:
+                    part = attr
+                else:
+                    from distutils.errors import DistutilsOptionError
+                    raise DistutilsOptionError(
+                        "version part options are mutually exclusive")
+        self.part = part or "patch"
+
+    def bumpversion(self, part, **kwargs):
+        """ Run bumpversion.main() with the specified arguments.
+        """
+        import bumpversion
+
+        args = ['--verbose'] if self.verbose > 1 else []
+        for k, v in kwargs.items():
+            k = "--{}".format(k.replace("_", "-"))
+            is_bool = isinstance(v, bool) and v is True
+            args.extend([k] if is_bool else [k, str(v)])
+        args.append(part)
+
+        log.debug(
+            "$ bumpversion %s" % " ".join(a.replace(" ", "\\ ") for a in args))
+
+        bumpversion.main(args)
+
+    def run(self):
+        log.info("bumping '%s' version" % self.part)
+        self.bumpversion(self.part)
+
+
+class release(bump_version):
+    """Drop the developmental release '.devN' suffix from the package version,
+    open the default text $EDITOR to write release notes, commit the changes
+    and generate a git tag.
+
+    Release notes can also be set with the -m/--message option, or by reading
+    from standard input.
+    """
+
+    description = "tag a new release"
+
+    user_options = [
+        ("message=", 'm', "message containing the release notes"),
+    ]
+
+    def initialize_options(self):
+        self.message = None
+
+    def finalize_options(self):
+        import re
+
+        current_version = self.distribution.metadata.get_version()
+        if not re.search(r"\.dev[0-9]+", current_version):
+            from distutils.errors import DistutilsSetupError
+            raise DistutilsSetupError(
+                "current version (%s) has no '.devN' suffix.\n       "
+                "Run 'setup.py bump_version' with any of "
+                "--major, --minor, --patch options" % current_version)
+
+        message = self.message
+        if message is None:
+            if sys.stdin.isatty():
+                # stdin is interactive, use editor to write release notes
+                message = self.edit_release_notes()
+            else:
+                # read release notes from stdin pipe
+                message = sys.stdin.read()
+
+        if not message.strip():
+            from distutils.errors import DistutilsSetupError
+            raise DistutilsSetupError("release notes message is empty")
+
+        self.message = "v{new_version}\n\n%s" % message
+
+    @staticmethod
+    def edit_release_notes():
+        """Use the default text $EDITOR to write release notes.
+        If $EDITOR is not set, use 'nano'."""
+        from tempfile import mkstemp
+        import os
+        import shlex
+        import subprocess
+
+        text_editor = shlex.split(os.environ.get('EDITOR', 'nano'))
+
+        fd, tmp = mkstemp(prefix='bumpversion-')
+        try:
+            os.close(fd)
+            with open(tmp, 'w') as f:
+                f.write("\n\n# Write release notes.\n"
+                        "# Lines starting with '#' will be ignored.")
+            subprocess.check_call(text_editor + [tmp])
+            with open(tmp, 'r') as f:
+                changes = "".join(
+                    l for l in f.readlines() if not l.startswith('#'))
+        finally:
+            os.remove(tmp)
+        return changes
+
+    def run(self):
+        log.info("stripping developmental release suffix")
+        # drop '.dev0' suffix, commit with given message and create git tag
+        self.bumpversion("release",
+                         tag=True,
+                         message="Release {new_version}",
+                         tag_message=self.message)
+
+
+needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+needs_bump2version = {'release', 'bump_version'}.intersection(sys.argv)
+bump2version = ['bump2version'] if needs_bump2version else []
 
 with open('README.rst', 'r', encoding='utf-8') as f:
     long_description = f.read()
@@ -35,6 +168,7 @@ setup(
             'fontmake = fontmake.__main__:main'
         ]
     },
+    setup_requires=wheel + bump2version,
     install_requires=[
         "fonttools>=3.9.2",
         "cu2qu>=1.1.1",
@@ -44,6 +178,10 @@ setup(
         "defcon>=0.3.1",
         "booleanOperations>=0.7.0",
     ],
+    cmdclass={
+        "release": release,
+        "bump_version": bump_version,
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         "Environment :: Console",
