86f26adbea72d059f15e35ebdb9b63571e9c1a26
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-14 22:37:34 +0100

    Update fonttools from 3.9.2 to 3.10.0
    new varLib added support for generating avar using the designspace <map> elements,
    and also for generating MVAR.

--- setup.py
+++ setup.py
@@ -170,7 +170,7 @@ setup(
     },
     setup_requires=wheel + bump2version,
     install_requires=[
-        "fonttools>=3.9.2",
+        "fonttools>=3.10.0",
         "cu2qu>=1.1.1",
         "glyphsLib>=1.6.0",
         "ufo2ft>=0.4.2",
