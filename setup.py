e7d62319dd3a20f93f75835989a1b2ee74463c26
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-04-14 22:33:19 +0100

    Update MutatorMath from 2.0.3 to 2.0.4

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ setup(
         "cu2qu>=1.1.1",
         "glyphsLib>=1.6.0",
         "ufo2ft>=0.4.2",
-        "MutatorMath>=2.0.3",
+        "MutatorMath>=2.0.4",
         "defcon>=0.3.1",
         "booleanOperations>=0.7.0",
     ],
