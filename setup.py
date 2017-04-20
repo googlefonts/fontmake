8f883e54d70af3e3e6da1d4972d153a9af64f697
Sascha Brawer <sascha@brawer.ch>
2017-04-20 09:46:58 +0200

    Update glyphsLib from 1.6.0 to 1.7.0
    Notable changes:
    
    * designspace documents now contain `<axes>` for variation fonts
    
    * the value range of the width axis now conforms to the OpenType spec
    
    * font instances can now specify custom PostScript font names
      by setting the `postscriptFontName` attribute in Glyhps

--- setup.py
+++ setup.py
@@ -172,7 +172,7 @@ setup(
     install_requires=[
         "fonttools>=3.10.0",
         "cu2qu>=1.1.1",
-        "glyphsLib>=1.6.0",
+        "glyphsLib>=1.7.0",
         "ufo2ft>=0.5.0",
         "MutatorMath>=2.0.4",
         "defcon>=0.3.1",
