42e0ebb0cc0dca957834fc4caff7d99ba742885f
Cosimo Lupo <cosimo@anthrotype.com>
2018-04-11 18:47:36 +0100

    ttfautohint: raise custom TTFAError exception if subprocess returns non-zero exit code

--- Lib/fontmake/errors.py
+++ Lib/fontmake/errors.py
@@ -2,3 +2,12 @@
 class FontmakeError(Exception):
     """Base class for all fontmake exceptions."""
     pass
+
+
+class TTFAError(FontmakeError):
+
+    def __init__(self, exitcode):
+        self.exitcode = exitcode
+
+    def __str__(self):
+        return "ttfautohint command failed: error " + str(self.exitcode)
