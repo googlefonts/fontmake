b2c790a8d22fb4c6412d32399c35ca583f520cfb
Cosimo Lupo <clupo@google.com>
2020-01-29 17:46:57 +0000

    Update requirements
    glyphsLib >= 5.1.3
    https://github.com/googlefonts/glyphsLib/releases/tag/v5.1.2
    https://github.com/googlefonts/glyphsLib/releases/tag/v5.1.3
    
    fonttools >= 4.2.5
    https://github.com/fonttools/fonttools/releases/tag/4.2.4
    https://github.com/fonttools/fonttools/releases/tag/4.2.5
    
    skia-pathops >= 0.2.1
    https://github.com/fonttools/skia-pathops/releases/tag/v0.2.1

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setup(
     setup_requires=wheel + ["setuptools_scm"],
     python_requires=">=3.6",
     install_requires=[
-        "fonttools[ufo,lxml,unicode]>=4.2.3",
+        "fonttools[ufo,lxml,unicode]>=4.2.5",
         "cu2qu>=1.6.6",
-        "glyphsLib>=5.1.1",
-        "ufo2ft>=2.11.0",
+        "glyphsLib>=5.1.3",
+        "ufo2ft>=2.11.1",
         "fontMath>=0.5.2",
         "booleanOperations>=0.9.0",
         "ufoLib2>=0.5.1",
