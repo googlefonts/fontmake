add7dbe08e727316400ebabb0896ab41974ec333
Simon Cozens <simon@simon-cozens.org>
2022-07-19 20:24:38 +0100

    A test for --check-compatibility and --no-check-compatibility

--- tests/test_compatibility.py
+++ tests/test_compatibility.py
@@ -1,7 +1,10 @@
+import pytest
 import ufoLib2
 from fontTools import designspaceLib
 
 from fontmake.compatibility import CompatibilityChecker
+from fontmake.__main__ import main
+from fontmake.errors import FontmakeError
 
 
 def test_compatibility_checker(data_dir, caplog):
@@ -24,3 +27,22 @@ def test_compatibility_checker(data_dir, caplog):
     assert (
         "Fonts had differing point type in glyph D, contour 0, point 10" in caplog.text
     )
+
+def test_compatibility_cli(data_dir, caplog):
+    ds = str(data_dir / "IncompatibleSans" / "IncompatibleSans.designspace")
+    with pytest.raises(SystemExit) as e_info:
+        main(["-o", "variable", "-m", ds])
+
+    main(["-o", "ttf", "-m", ds])
+
+    with pytest.raises(SystemExit) as e_info:
+        main(["--check-compatibility", "-o", "ttf", "-m", ds])
+
+    # We stopped things before they got to the cu2qu level
+    assert "cu2qu.ufo" not in caplog.text
+
+    with pytest.raises(SystemExit):
+        main(["--no-check-compatibility", "-o", "variable", "-m", ds])
+
+    # Things got to the cu2qu level (i.e. compatibility checker did not run)
+    assert "cu2qu.ufo" in caplog.text
