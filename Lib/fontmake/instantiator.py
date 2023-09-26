7cc6c8d44a584d61732e87fc8b810cfa4fe9beff
Yanone <post@yanone.de>
2023-09-26 14:51:19 +0200

    Check for existing swap to not add twice

--- Lib/fontmake/instantiator.py
+++ Lib/fontmake/instantiator.py
@@ -168,7 +168,7 @@ def process_rules_swaps(rules, location, glyphNames):
                 # I guess it should be, so that we can swap, and if it isn't,
                 # then it's better to error out later when we try to swap,
                 # instead of silently ignoring the rule here.
-                if oldName in glyphNames:
+                if oldName in glyphNames and (oldName, newName) not in swaps:
                     swaps.append((oldName, newName))
     return swaps
 
